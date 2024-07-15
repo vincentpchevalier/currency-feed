661ab6503c6d97712b1b6d78cde6c4ca4392d3e1
vincentpchevalier <vincentpchevalier@proton.me>
2024-07-15 13:42:14 -0400

    build fetch currency name function in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyExchanges from './CurrencyExchanges';
 import CurrencyInput from './CurrencyInput';
 
+const COUNTRY_API_URL = 'https://restcountries.com/v3.1/';
+
 function App() {
-	const [country, setCountry] = useState('Germany');
-	const [currencyCode, setCurrencyCode] = useState('EUR');
+	const [currencyCode, setCurrencyCode] = useState('');
+	const [country, setCountry] = useState('');
+	const [amount, setAmount] = useState('');
+
+	useEffect(
+		function () {
+			async function fetchCountry() {
+				if (country.length < 3) return; // to make sure not too many calls get made to the API
+				try {
+					const res = await fetch(`${COUNTRY_API_URL}name/${country}`);
+
+					if (!res.ok)
+						throw new Error('Something went wrong with fetching the country.');
+
+					const data = await res.json();
+
+					// console.log(Object.keys(data[0].currencies)[0]);
+
+					const currencyCode = Object.keys(data[0].currencies)[0];
+
+					setCurrencyCode(currencyCode);
+				} catch (err) {
+					console.warn(err);
+				} finally {
+					// setIsLoading()
+				}
+			}
+			fetchCountry();
+		},
+		[country]
+	);
+
 	return (
 		<>
 			<h1>Current Currencies</h1>
-			<CurrencyInput country={country} code={currencyCode} />
+			<CurrencyInput
+				country={country}
+				code={currencyCode}
+				amount={amount}
+				setCountry={setCountry}
+				setAmount={setAmount}
+			/>
 			<CurrencyExchanges />
 		</>
 	);
